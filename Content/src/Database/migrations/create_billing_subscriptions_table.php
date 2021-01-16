c05ce893c014e093bf5d67fc69ad8b5047582b0f
Malcolm Paul <malcolm@ninjasitm.com>
2021-01-16 16:27:53 -0500

    Changed the name of the subscription tables

--- Content/src/Database/migrations/create_billing_subscriptions_table.php
+++ Content/src/Database/migrations/create_billing_subscriptions_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateSubscriptionsTable extends Migration
+class CreateBiullingSubscriptionsTable extends Migration
 {
     /**
      * Run the migrations.
