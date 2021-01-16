0b276e07b7049f4210cc2f6ff3edbf85a50845dd
Malcolm Paul <malcolm@ninjasitm.com>
2021-01-16 14:52:40 -0500

    Making user id field nullable

--- Content/src/Database/migrations/create_invitations_table.php
+++ Content/src/Database/migrations/create_invitations_table.php
@@ -12,15 +12,17 @@ class CreateInvitationsTable extends Migration
      */
     public function up()
     {
-        Schema::create('invitations', function (Blueprint $table) {
-            $table->string('id')->primary();
-            $table->unsignedBigInteger('team_id')->index();
-            $table->unsignedBigInteger('user_id')->nullable()->index();
-            $table->string('role')->nullable();
-            $table->string('email');
-            $table->string('token', 40)->unique();
-            $table->timestamps();
-        });
+        Schema::create(
+            'invitations', function (Blueprint $table) {
+                $table->string('id')->primary();
+                $table->unsignedBigInteger('team_id')->nullable()->index();
+                $table->unsignedBigInteger('user_id')->nullable()->index();
+                $table->string('role')->nullable();
+                $table->string('email');
+                $table->string('token', 40)->unique();
+                $table->timestamps();
+            }
+        );
     }
 
     /**
