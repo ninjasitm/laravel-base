6a06c23ed96b55a9bcc682bc24ab5bd740ddacfd
Malcolm Paul <malcolm@ninjasitm.com>
2021-01-31 17:11:35 -0500

    BUgfixes

--- Content/src/Database/migrations/2019_09_20_000000_create_category_table.php
+++ Content/src/Database/migrations/2019_09_20_000000_create_category_table.php
@@ -8,29 +8,31 @@ class CreateCategoryTable extends Migration
     public function up()
     {
         if (!Schema::hasTable('categories')) {
-            Schema::create('categories', function ($table) {
-                $table->engine = 'InnoDB';
-                $table->increments('id');
-                $table->text('title')->nullable();
-                $table->text('slug')->nullable();
-                $table->text('description')->nullable();
-                $table->text('photo_url')->nullable();
-                $table->integer('author_id');
-                $table->integer('editor_id')->nullable();
-                $table->timestamp('updated_at')->nullable();
-                $table->timestamp('created_at')->nullable();
-                $table->integer('deleter_id')->nullable();
-                $table->integer('parent_id')->nullable();
-                $table->integer('nest_left')->nullable();
-                $table->integer('nest_right')->nullable();
-                $table->integer('nest_depth')->nullable();
-                $table->softDeletes();
+            Schema::create(
+                'categories', function ($table) {
+                    $table->engine = 'InnoDB';
+                    $table->increments('id');
+                    $table->text('title')->nullable();
+                    $table->text('slug')->nullable();
+                    $table->text('description')->nullable();
+                    $table->text('photo_url')->nullable();
+                    $table->integer('author_id')->nullable();
+                    $table->integer('editor_id')->nullable();
+                    $table->timestamp('updated_at')->nullable();
+                    $table->timestamp('created_at')->nullable();
+                    $table->integer('deleter_id')->nullable();
+                    $table->integer('parent_id')->nullable();
+                    $table->integer('nest_left')->nullable();
+                    $table->integer('nest_right')->nullable();
+                    $table->integer('nest_depth')->nullable();
+                    $table->softDeletes();
 
-                $table->foreign('author_id')->references('id')->on('users')->onDelete('cascade');
-                $table->foreign('editor_id')->references('id')->on('users')->onDelete('cascade');
-                $table->foreign('deleter_id')->references('id')->on('users')->onDelete('cascade');
-                $table->foreign('parent_id')->references('id')->on('categories')->onDelete('cascade');
-            });
+                    $table->foreign('author_id')->references('id')->on('users')->onDelete('cascade');
+                    $table->foreign('editor_id')->references('id')->on('users')->onDelete('cascade');
+                    $table->foreign('deleter_id')->references('id')->on('users')->onDelete('cascade');
+                    $table->foreign('parent_id')->references('id')->on('categories')->onDelete('cascade');
+                }
+            );
         }
     }
 
--- Content/src/Database/migrations/2019_09_20_000001_create_nitm_events.php
+++ Content/src/Database/migrations/2019_09_20_000001_create_nitm_events.php
@@ -10,41 +10,47 @@ class CreateNitmEvents extends Migration
     public function up()
     {
         if (!Schema::hasTable('nitm_events')) {
-            Schema::create('nitm_events', function ($table) {
-                $table->engine = 'InnoDB';
-                $table->increments('id');
-                $table->boolean('is_free')->default(1);
-                $table->string('title');
-                $table->string('slug');
-                $table->integer('type_id')->nullable();
-                $table->integer('category_id')->nullable();
-                $table->integer('author_id');
-                $table->timestamp('created_at');
-                $table->integer('editor_d')->nullable();
-                $table->timestamp('updated_at')->nullable();
-                $table->dateTime('starts_at');
-                $table->dateTime('ends_at')->nullable();
-                $table->dateTime('postponed_to');
-                $table->text('description');
-                $table->string('status', 10);
-                $table->double('cost', 10, 2)->nullable();
-                $table->integer('location_id');
-                $table->unique('slug');
-                $table->foreign('location_id')->references('id')->on('nitm_location');
-                $table->softDeletes();
-            });
+            Schema::create(
+                'nitm_events', function ($table) {
+                    $table->engine = 'InnoDB';
+                    $table->increments('id');
+                    $table->boolean('is_free')->default(1);
+                    $table->string('title');
+                    $table->string('slug');
+                    $table->integer('type_id')->nullable();
+                    $table->integer('category_id')->nullable();
+                    $table->integer('author_id')->nullable();
+                    $table->timestamp('created_at');
+                    $table->integer('editor_d')->nullable();
+                    $table->timestamp('updated_at')->nullable();
+                    $table->dateTime('starts_at');
+                    $table->dateTime('ends_at')->nullable();
+                    $table->dateTime('postponed_to');
+                    $table->text('description');
+                    $table->string('status', 10);
+                    $table->double('cost', 10, 2)->nullable();
+                    $table->integer('location_id');
+                    $table->unique('slug');
+                    $table->foreign('location_id')->references('id')->on('nitm_location');
+                    $table->softDeletes();
+                }
+            );
 
-            \Nitm\Content\Models\Category::create([
+            \Nitm\Content\Models\Category::create(
+                [
                 'title' => 'Event Type',
                 'description' => 'Event Type',
                 'author_id' => 1,
-            ]);
+                ]
+            );
 
-            \Nitm\Content\Models\Category::create([
+            \Nitm\Content\Models\Category::create(
+                [
                 'title' => 'Event Category',
                 'description' => 'Event Category',
                 'author_id' => 1,
-            ]);
+                ]
+            );
         }
     }
 
