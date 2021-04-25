520d7312c8edd029725ab826bdaa96e7a28a70f8
Malcolm Paul <malcolm@ninjasitm.com>
2021-04-25 17:41:14 -0400

    Added testing traits

---
+++ Testing/src/ApiTestTrait.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Tests;
+
+use Illuminate\Support\Arr;
+
+trait ApiTestTrait
+{
+
+    /**
+     * @inheritDoc
+     */
+    public function json($method, $url, array $data = [], array $headers = [])
+    {
+        $url = ltrim($url, '/');
+        $apiUrl = trim($this->getApiBase($url), '/');
+        echo "Url: " . strtoupper($method) . " /{$apiUrl}\n";
+        return parent::json($method, $apiUrl, $data, $headers);
+    }
+
+    /**
+     * @return [type]
+     */
+    public function getApiBase($url = '')
+    {
+
+        if ($this->usesTeams) {
+            $teamId = $this->team ? "{$this->team->id}" : "";
+            return "{$this->apiBase}/teams/{$teamId}/" . str_replace("{$this->apiBase}", '', str_replace(ltrim($this->apiBase, '/') . '/', '', $url));
+        }
+        return str_replace("{$this->apiBase}{$this->apiBase}", $this->apiBase, "{$this->apiBase}/{$url}");
+    }
+
+    public function assertApiResponse(array $actualData)
+    {
+        $this->assertApiSuccess();
+
+        $response = json_decode($this->response->getContent(), true);
+        $responseData = $response['data'];
+        $id = Arr::get($responseData, 'id') ?? Arr::get($responseData, 'uuid');
+        $this->assertNotEmpty($id);
+        $this->assertModelData($actualData, $responseData);
+    }
+
+    public function assertApiSuccess()
+    {
+        $this->response->assertJson(['success' => true]);
+    }
+
+    public function assertModelData(array $actualData, array $expectedData)
+    {
+        foreach ($actualData as $key => $value) {
+            $this->assertEquals(Arr::get($actualData, $key, '(not set)'), Arr::get($expectedData, $key, '(not set)'));
+        }
+    }
+}
---
+++ Testing/src/CreatesApplication.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Tests;
+
+use Artisan;
+use Illuminate\Contracts\Console\Kernel;
+use App\Providers\JetstreamServiceProvider;
+
+trait CreatesApplication
+{
+    /**
+     * Creates the application.
+     *
+     * @return \Illuminate\Foundation\Application
+     */
+    public function createApplication()
+    {
+        $app = include __DIR__ . '/../bootstrap/app.php';
+
+        $app->make(Kernel::class)->bootstrap();
+        return $app;
+    }
+
+    /**
+     * registerServiceProviders
+     *
+     * @return void
+     */
+    protected function registerServiceProviders()
+    {
+        // Needed for team tests
+        app()->register(JetstreamServiceProvider::class);
+    }
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        // Artisan::call('migrate:refresh');
+        Artisan::call('config:clear');
+        Artisan::call('cache:clear');
+        // Artisan::call('migrate');
+        Artisan::call('db:seed');
+
+        $this->registerServiceProviders();
+
+        $teamClass = config('nitm-content.team_model');
+        if($this->usesTeams && class_exists($teamClass)) {
+            $this->setupTeam(factory($teamClass)->create());
+        }
+
+        $userClass = config('nitm-content.user_model');
+        auth()->login($userClass::first());
+    }
+
+    public function tearDown(): void
+    {
+        // Artisan::call('migrate:reset');
+        parent::tearDown();
+    }
+}
