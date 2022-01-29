6a5b5fb8c212ee25562af97bd60b5c75ae4e3e11
Malcolm Paul <malcolm@ninjasitm.com>
2022-01-29 16:43:41 -0500

    Supporting updated factories for all base models

--- Content/src/Models/BaseModel.php
+++ Content/src/Models/BaseModel.php
@@ -2,34 +2,36 @@
 
 namespace Nitm\Content\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model as EloquentModel;
-use Nitm\Content\Traits\Search;
 use Nitm\Content\Traits\CustomWith;
-use Nitm\Content\Traits\SyncsRelations;
-use Nitm\Content\Traits\Model as ModelTrait;
-use Nitm\Content\Traits\HasFiles;
-use Nitm\Content\Traits\HasMetadata;
-use Nitm\Content\Traits\SetUserId;
 use Nitm\Content\Traits\DatesTimezoneConversion;
 use Nitm\Content\Traits\FiltersModels;
 use Nitm\Content\Traits\FiltersUsers;
-use Nitm\Content\Traits\ProvidesUrls;
+use Nitm\Content\Traits\HasFiles;
+use Nitm\Content\Traits\HasMetadata;
 use Nitm\Content\Traits\HasTimestamps;
+use Nitm\Content\Traits\Model as ModelTrait;
+use Nitm\Content\Traits\ProvidesUrls;
+use Nitm\Content\Traits\Search;
+use Nitm\Content\Traits\SetUserId;
+use Nitm\Content\Traits\SyncsRelations;
 
 class BaseModel extends EloquentModel
 {
     use ModelTrait,
-        HasTimestamps,
-        Search,
-        CustomWith,
-        SyncsRelations,
-        HasFiles,
-        HasMetadata,
-        SetUserId,
-        FiltersModels,
-        FiltersUsers,
-        DatesTimezoneConversion,
-        ProvidesUrls;
+    HasTimestamps,
+    Search,
+    CustomWith,
+    SyncsRelations,
+    HasFiles,
+    HasMetadata,
+    SetUserId,
+    FiltersModels,
+    FiltersUsers,
+    DatesTimezoneConversion,
+    ProvidesUrls,
+        HasFactory;
 
     public function fromJson($value, $asObject = false)
     {
