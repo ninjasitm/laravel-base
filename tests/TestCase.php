58531575423435ad44c44a6d32b8e93b4708edb3
Malcolm Paul <malcolm@ninjasitm.com>
2026-04-02 23:06:35 -0400

    style: apply pint formatting to tests/TestCase.php

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,44 +1,54 @@
 <?php
+
 namespace Tests;
 
-use function Orchestra\Testbench\artisan;
+use Illuminate\Foundation\Application;
 use Nitm\Content\Models\User;
 use Nitm\Content\NitmContent;
 use Nitm\Content\NitmContentServiceProvider;
 use Nitm\Testing\ApiTestTrait;
 use Nitm\Testing\PackageTestCase as BaseTestCase;
 
-abstract class TestCase extends BaseTestCase {
+use function Orchestra\Testbench\artisan;
+
+abstract class TestCase extends BaseTestCase
+{
     use ApiTestTrait;
+
     private $dbMigrated = false;
 
-    protected function getPackageProviders($app) {
+    protected function getPackageProviders($app)
+    {
         return [
             NitmContentServiceProvider::class,
         ];
     }
+
     /**
      * Define environment setup.
      *
-     * @param  \Illuminate\Foundation\Application  $app
+     * @param  Application  $app
      * @return void
      */
-    protected function defineEnvironment($app) {
+    protected function defineEnvironment($app)
+    {
         NitmContent::useUserModel(User::class);
         $app['config']->set('database.default', 'sqlite');
         $app['config']->set('database.connections.sqlite', [
-            'driver'   => 'sqlite',
+            'driver' => 'sqlite',
             'database' => ':memory:',
-            'prefix'   => '',
+            'prefix' => '',
         ]);
     }
-    protected function defineDatabaseMigrations() {
+
+    protected function defineDatabaseMigrations()
+    {
         if (! $this->dbMigrated) {
             $this->artisan('migrate:fresh', ['--database' => 'sqlite']);
             $this->dbMigrated = true;
 
             $this->beforeApplicationDestroyed(
-                fn() => artisan($this, 'migrate:rollback', ['--database' => 'sqlite'])
+                fn () => artisan($this, 'migrate:rollback', ['--database' => 'sqlite'])
             );
         }
     }
